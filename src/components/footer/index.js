b4e02612f646d19f375ae37e676b9188c02bdfb8
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-08-27 16:51:39 +0200

    Add header and footer components, connect to the rest api

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <a
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Powered by{' '}
+        </a>
+      </footer>
+    </>
+  );
+}
