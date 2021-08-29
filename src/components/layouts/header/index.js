a22a9bce4cf7718331c56c94120bf0d084e0adc4
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-08-29 16:14:30 +0200

    Add dynamic siteLogoUrl, siteTitle and siteDescription

--- src/components/layouts/header/index.js
+++ src/components/layouts/header/index.js
@@ -1,29 +1,44 @@
 import Head from 'next/head';
 
-export default function Header() {
+export default function Header({ header }) {
+  const { headerMenuItems, siteDescription, siteLogoUrl, siteTitle, favicon } =
+    header || {};
+
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>{siteTitle || 'Nextjs Woocommerce'}</title>
+        <link rel='icon' href={favicon || '/favicon.ico'} />
       </Head>
       <div className='header'>
         <nav className='bg-white p-4'>
           <div className='flex items-center justify-between flex-wrap container mx-auto'>
             <div className='flex items-center flex-shrink-0 text-black mr-20'>
-              <svg
-                className='fill-current h-8 w-8 mr-2'
-                width='54'
-                height='54'
-                viewBox='0 0 54 54'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z'></path>
-              </svg>
-              <span className='font-semibold text-xl tracking-tight'>
-                <a className='' href='/'>
+              {siteLogoUrl ? (
+                <img
+                  className='mr-2'
+                  src={siteLogoUrl}
+                  alt={`${siteTitle} logo`}
+                  width='80'
+                  height='80'
+                />
+              ) : (
+                <svg
+                  className='fill-current h-8 w-8 mr-2'
+                  width='54'
+                  height='54'
+                  viewBox='0 0 54 54'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z'></path>
+                </svg>
+              )}
+              <span>
+                <a className='font-semibold text-xl tracking-tight' href='/'>
+                  {siteTitle || 'WooNext'}
                   WooNext
                 </a>
+                {siteDescription ? <p>{siteDescription}</p> : null}
               </span>
             </div>
             <div className='block lg:hidden'>
