d56935e359a584cd87d659316da30fafc78de5d6
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-09-09 15:38:13 +0200

    Add the woocommerce rest api package, add the api endpoint

---
+++ pages/api/get-products.js
@@ -0,0 +1,31 @@
+const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default;
+
+const api = new WooCommerceRestApi({
+  url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+  consumerKey: process.env.WC_CONSUMER_KEY,
+  consumerSecret: process.env.WC_CONSUMER_SECRET,
+  version: 'wc/v3',
+});
+
+export default async function handler(req, res) {
+  const responseData = {
+    success: false,
+    products: [],
+  };
+
+  const { perPage } = req?.query ?? {};
+
+  try {
+    const { data } = await api.get('products', {
+      per_page: perPage || 50,
+    });
+
+    responseData.success = true;
+    responseData.products = data;
+
+    res.json(responseData);
+  } catch (error) {
+    responseData.error = error.message;
+    res.status(500).json(responseData);
+  }
+}
